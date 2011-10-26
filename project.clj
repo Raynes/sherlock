57a57d7d1fe547338541390b9f9ff570c7192deb
Anthony Grimes <i@raynes.me>
2011-10-26 02:06:42 -0500

    0.1.0-alpha1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sherlock "0.1.0-SNAPSHOT"
+(defproject sherlock "0.1.0-alpha1"
   :description "A library for searching maven repositories."
   :dependencies [[clojure "1.2.1"]
                  [clucy "0.2.2"]])
